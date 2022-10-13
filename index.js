c5ea6f569e6b55a627ac8e572fa6af142b28ed11
Robert0101 <rob.tangled@gmail.com>
2022-10-13 14:03:35 -0300

    refactor/Modularizar código

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// index.js
-// where your node app starts
 require("dotenv").config();
-const apiTest = require("./routes/api-Route");
 const morgan = require("morgan");
+const PORT = process.env.PORT || 3001;
+
+const apiRouter = require("./routes/api-Route");
 // init project
 var express = require("express");
 var app = express();
@@ -15,74 +15,16 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 app.use(morgan("dev"));
-// app.use("/api", apiTest);
+
+//Use as middleware the routes for "/api" in ./routes/api-Routes.js
+app.use("/api", apiRouter);
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
-});
-
-app.get("/api/:date", (req, res) => {
-  try {
-    let dateFromReq = req.params.date;
-
-    const parsedDateToUnix = Date.parse(dateFromReq);
-    console.log("Date.parse(dateFromReq)");
-    console.log(parsedDateToUnix);
-
-    if (!parsedDateToUnix) {
-      console.log("parsedDateToUnix es falso");
-      let dateMiliseconds = Number(dateFromReq); //Lo transformo de string a número
-      if (!Number(dateFromReq)) {
-        console.log(`dateFromReq a Number es falso:`);
-        return res.send({ error: "Invalid Date" });
-      }
-      const dateObject = new Date(dateMiliseconds);
-
-      return res
-        .status(200)
-        .send({ unix: Number(dateFromReq), utc: dateObject.toUTCString() });
-    }
-
-    const nuevaFecha = new Date(parsedDateToUnix);
-    console.log(`new Date(parsedDateToUnix)`);
-    console.log(nuevaFecha);
-
-    let date = Date(dateFromReq);
-    console.log("Date(dateFromReq)");
-    console.log(date);
-    console.log(new Date(date).toUTCString());
-
-    return res.status(200).send({
-      unix: parsedDateToUnix,
-      utc: nuevaFecha.toUTCString(),
-    });
-  } catch (error) {
-    console.log(`Error en /api/:date. ${error.message}`);
-    return res.send(error.message);
-  }
-});
-
-app.get("/api", (req, res) => {
-  try {
-    let unixTime = "";
-    console.log(Date.now());
-    let utcTime = new Date();
-    console.log(utcTime);
-    console.log(utcTime.toUTCString());
-
-    return res.json({ unix: Date.now(), utc: utcTime.toUTCString() });
-  } catch (error) {
-    console.log(`Error en /api. ${error.message}`);
-  }
-});
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
--- routes/api-Route.js
+++ routes/api-Route.js
@@ -1,9 +1,58 @@
 const router = require("express").Router();
-// const router = Router();
 
-router.get("/api/test", (req, res) => {
+// ------ RUTAS : -------
+
+router.get("/test", (req, res) => {
   console.log("api/test de prueba");
   return res.status(200).send("HOLA!!!");
+});
+
+// your first API endpoint...
+router.get("/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+router.get("/:date", (req, res) => {
+  try {
+    const dateFromReq = req.params.date;
+    const parsedDateToUnix = Date.parse(dateFromReq);
+
+    if (!parsedDateToUnix) {
+      console.log("parsedDateToUnix es falso");
+      let dateMiliseconds = Number(dateFromReq); //Lo transformo de string a número
+      if (!Number(dateFromReq)) {
+        console.log(`dateFromReq a Number es falso:`);
+        return res.send({ error: "Invalid Date" });
+      }
+      const dateObject = new Date(dateMiliseconds);
+
+      return res
+        .status(200)
+        .send({ unix: Number(dateFromReq), utc: dateObject.toUTCString() });
+    }
+
+    const nuevaFecha = new Date(parsedDateToUnix);
+
+    return res.status(200).send({
+      unix: parsedDateToUnix,
+      utc: nuevaFecha.toUTCString(),
+    });
+  } catch (error) {
+    console.log(`Error en /api/:date. ${error.message}`);
+    return res.send(error.message);
+  }
+});
+
+router.get("/", (req, res) => {
+  console.log(`En "api/"`);
+  try {
+    let utcTime = new Date();
+
+    return res.json({ unix: Date.now(), utc: utcTime.toUTCString() });
+  } catch (error) {
+    console.log(`Error en /api. ${error.message}`);
+    return res.status(400).send({ error: `${error.message}` });
+  }
 });
 
 module.exports = router;
